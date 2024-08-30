e8737f6d284d87c8b99ed4824440132bf8ba6fa2
kohlerjoao <104786374+kohlerjoao@users.noreply.github.com>
2024-08-30 10:53:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,98 @@
+const quizData = [
+    {
+        question: "qual o jogo mais vendido?",
+        a: "minecraft",
+        b: "gta",
+        c: "mario",
+        d: "pou",
+        correct: "d",
+    },
+    {
+        question: "qual é mais famoso?",
+        a: "ribamar",
+        b: "cr7",
+        c: "leo pele",
+        d: "veio da avan",
+        correct: "b",
+    },
+    {
+        question: "qual maior o jogador?",
+        a: "LEO PELE",
+        b: "messi",
+        c: "gustavo lima",
+        d: "mano gusta",
+        correct: "a",
+    },
+    {
+        question: "qual a cor do ceu?",
+        a: "nao tem cor",
+        b: "azul",
+        c: "branco",
+        d: "none of the above",
+        correct: "b",
+    },
+];
+
+const quiz = document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
+
+let currentQuiz = 0
+let score = 0
+
+loadQuiz()
+
+function loadQuiz() {
+    deselectAnswers()
+
+    const currentQuizData = quizData[currentQuiz]
+
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+}
+
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+}
+
+function getSelected() {
+    let answer
+
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+
+    return answer
+}
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    
+    if(answer) {
+        if(answer === quizData[currentQuiz].correct) {
+            score++
+        }
+
+        currentQuiz++
+
+        if(currentQuiz < quizData.length) {
+            loadQuiz()
+        } else {
+            quiz.innerHTML = `
+                <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+
+                <button onclick="location.reload()">Reload</button>
+            `
+        }
+    }
+})
